127b6a67aa39224e7f42a08e17a42772320fdf6f
Elliott Roche <eroche91@gmail.com>
2019-08-07 10:34:29 -0500

    removing floating action button as a standalone component

--- development-playgroud/index.tsx
+++ development-playgroud/index.tsx
@@ -1,12 +1,25 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { CssFramework, Avatar } from "../dist/";
+import {
+  CssFramework,
+  Color,
+  Buttons,
+  Typography,
+  ProgressBar
+} from "../dist/";
 
 const App = () => {
   return (
     <div className="flex justify-center items-center h-100">
       <CssFramework />
-      <Avatar></Avatar>
+      <Buttons.FloatingAction
+        icon={"back"}
+        iconColor={Color.green500}
+        size={50}
+        backgroundColor={Color.green100}
+        elevation={24}
+        type="submit"
+      />
     </div>
   );
 };
