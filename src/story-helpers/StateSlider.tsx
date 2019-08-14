6db8b2da3eaf6c66f5b8011ca4589cf76fee8cdf
Jack Spiva <johnspiva@gmail.com>
2019-08-14 16:01:43 -0500

    Change initial state to default state

---
+++ src/story-helpers/StateSlider.tsx
@@ -0,0 +1,38 @@
+import React, { Fragment, useState } from "react";
+import Toggle from "../components/Inputs/Toggle";
+
+interface IRenderProps {
+  rangeLevel: number;
+}
+
+interface IProps {
+  label?: string;
+  children: (props: IRenderProps) => JSX.Element;
+  defaultState?: number;
+}
+
+export default function StateSlider(props: IProps) {
+  const { children, label = "", defaultState = 0 } = props;
+  const [rangeLevel, setState] = useState(defaultState);
+
+  return (
+    <Fragment>
+      <div>
+        <label>
+          {`Adjust ${label}`}
+          <div className="mt-05">
+            <input
+              type="range"
+              className="my-05"
+              min="0"
+              max="100"
+              value={rangeLevel}
+              onChange={({ target }) => setState(parseInt(target.value))}
+            />
+          </div>
+        </label>
+      </div>
+      {children({ rangeLevel })}
+    </Fragment>
+  );
+}
