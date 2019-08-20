45c8720d87c0bdda6cd2112b956e92b51616b9ec
Elliott Roche <eroche91@gmail.com>
2019-08-20 16:46:37 -0500

    making statistic sizes more varied

--- src/components/Statistic/index.tsx
+++ src/components/Statistic/index.tsx
@@ -1,118 +1,109 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import Typography from "../Typography";
 import FormatMoney from "../FormatMoney";
 import FormatNumber from "../FormatNumber";
 import FormatPercent from "../FormatPercent";
+import { Color } from "src";
 
 export interface IStatisticProps {
-  size?: "tiny" | "small" | "regular" | "large" | "xlarge" | "huge";
+  size?: "small" | "regular" | "large" | "xlarge" | "huge";
   /**
    * The formatting that should be applied to the Statistics value
    */
   format: "money" | "number" | "percentage" | "text";
   label: string;
   value: number | string;
+  color?: Color;
 }
 
-export default class Statistic extends PureComponent<IStatisticProps> {
-  static defaultProps = {
-    size: "regular"
-  };
-
-  formattedValue = (value: any, format: any) => {
+export default function Statistic({
+  size = "regular",
+  format,
+  label,
+  value,
+  color = Color.black500
+}: IStatisticProps) {
+  const formattedValue = (val: any) => {
     switch (format) {
       case "money":
-        return <FormatMoney value={value} />;
+        return <FormatMoney value={val} />;
       case "number":
-        return <FormatNumber value={value} />;
+        return <FormatNumber value={val} />;
       case "percentage":
-        return <FormatPercent value={value} />;
+        return <FormatPercent value={val} />;
       case "text":
         return value;
       default:
-        return <FormatNumber value={value} />;
+        return <FormatNumber value={val} />;
     }
   };
 
-  render() {
-    const { label, size, value, format } = this.props;
-
-    switch (size) {
-      case "tiny":
-        return (
-          <div className="flex flex-column justify-center items-center">
-            <Typography.Small>
-              {this.formattedValue(value, format)}
-            </Typography.Small>
-            <Typography.Tiny uppercase muted>
-              {label}
-            </Typography.Tiny>
-          </div>
-        );
-      case "small":
-        return (
-          <div className="flex flex-column justify-center items-center">
-            <Typography.Small>
-              {this.formattedValue(value, format)}
-            </Typography.Small>
-            <Typography.Small uppercase muted>
-              {label}
-            </Typography.Small>
-          </div>
-        );
-      case "regular":
-        return (
-          <div className="flex flex-column justify-center items-center">
-            <Typography>{this.formattedValue(value, format)}</Typography>
-            <Typography.Small uppercase muted>
-              {label}
-            </Typography.Small>
-          </div>
-        );
-      case "large":
-        return (
-          <div className="flex flex-column justify-center items-center">
-            <Typography.Large>
-              {this.formattedValue(value, format)}
-            </Typography.Large>
-            <Typography.Small uppercase muted>
-              {label}
-            </Typography.Small>
-          </div>
-        );
-      case "xlarge":
-        return (
-          <div className="flex flex-column justify-center items-center">
-            <Typography.HeadingSmall weight={600}>
-              {this.formattedValue(value, format)}
-            </Typography.HeadingSmall>
-            <Typography uppercase muted>
-              {label}
-            </Typography>
-          </div>
-        );
-      case "huge":
-        return (
-          <div className="flex flex-column justify-center items-center">
-            <Typography.HeadingLarge weight={600}>
-              {this.formattedValue(value, format)}
-            </Typography.HeadingLarge>
-            <Typography uppercase muted>
-              {label}
-            </Typography>
-          </div>
-        );
-      default:
-        return (
-          <div className="flex flex-column justify-center items-center">
-            <Typography weight={600}>
-              {this.formattedValue(value, format)}
-            </Typography>
-            <Typography.Small uppercase muted>
-              {label}
-            </Typography.Small>
-          </div>
-        );
-    }
+  switch (size) {
+    case "small":
+      return (
+        <div className="flex flex-column justify-center items-center">
+          <Typography.Small color={color} weight={600}>
+            {formattedValue(value)}
+          </Typography.Small>
+          <Typography.Small uppercase muted>
+            {label}
+          </Typography.Small>
+        </div>
+      );
+    case "regular":
+      return (
+        <div className="flex flex-column justify-center items-center">
+          <Typography.Large color={color} weight={600}>
+            {formattedValue(value)}
+          </Typography.Large>
+          <Typography uppercase muted>
+            {label}
+          </Typography>
+        </div>
+      );
+    case "large":
+      return (
+        <div className="flex flex-column justify-center items-center">
+          <Typography.HeadingTiny color={color} weight={600}>
+            {formattedValue(value)}
+          </Typography.HeadingTiny>
+          <Typography.Large uppercase muted>
+            {label}
+          </Typography.Large>
+        </div>
+      );
+    case "xlarge":
+      return (
+        <div className="flex flex-column justify-center items-center">
+          <Typography.HeadingLarge weight={600} color={color}>
+            {formattedValue(value)}
+          </Typography.HeadingLarge>
+          <Typography.Large uppercase muted>
+            {label}
+          </Typography.Large>
+        </div>
+      );
+    case "huge":
+      return (
+        <div className="flex flex-column justify-center items-center">
+          <Typography.Subtitle weight={600} color={color}>
+            {formattedValue(value)}
+          </Typography.Subtitle>
+          <Typography.HeadingTiny uppercase muted>
+            {label}
+          </Typography.HeadingTiny>
+        </div>
+      );
+    default:
+      return (
+        <div className="flex flex-column justify-center items-center">
+          <Typography.Large color={color} weight={600}>
+            {formattedValue(value)}
+          </Typography.Large>
+          <Typography uppercase muted>
+            {label}
+          </Typography>
+        </div>
+      );
   }
 }
