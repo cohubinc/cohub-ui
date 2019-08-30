63d8bd1dd13e7782056260a20be0bc8140ce8d10
brayhoward <brandonrhoward@gmail.com>
2019-08-29 19:41:31 -0500

    Update checkbox types so it can be used  without final form

--- src/components/Inputs/Checkbox/index.tsx
+++ src/components/Inputs/Checkbox/index.tsx
@@ -1,45 +1,46 @@
 import React from "react";
 import { FieldRenderProps } from "react-final-form";
-import "./Checkbox.scss";
+
 import Icon from "src/components/Icon";
 import Color from "src/definitions/enums/Color";
 
+import "./Checkbox.scss";
+
+type FieldProps = FieldRenderProps<boolean | string, HTMLInputElement>;
 interface ICheckboxProps {
   label: string;
+  input?: Partial<FieldProps["input"]>;
+  meta?: FieldProps["meta"];
 }
 
-export type TCheckboxProps = ICheckboxProps &
-  FieldRenderProps<boolean | string, HTMLInputElement>;
+export type TCheckboxProps = ICheckboxProps;
 
-export default class Checkbox extends React.Component<TCheckboxProps> {
-  render() {
-    const { label, input } = this.props;
+export default function({ label, input = {} }: TCheckboxProps) {
+  const checked =
+    (input.value && input.value === true) || input.value === "true";
 
-    const checked = input.value === true || input.value === "true";
+  const toggle = () => {
+    input.onChange && input.onChange(!checked as any);
+  };
 
-    const toggle = () => {
-      input.onChange(!checked as any);
-    };
+  const keyDown = (evt: any) => {
+    if (evt.keyCode && evt.keyCode === 32) {
+      toggle();
+    }
+  };
 
-    const keyDown = (evt: any) => {
-      if (evt.keyCode && evt.keyCode === 32) {
-        toggle();
-      }
-    };
-
-    return (
-      <div className="CheckboxField-Container flex justify-start items-center">
-        <div
-          className="CheckboxField mr-05"
-          role="checkbox"
-          tabIndex={0}
-          onKeyDown={keyDown}
-          onClick={toggle}
-        >
-          {checked && <Icon.Checkmark color={Color.primary} size={16} />}
-        </div>
-        <label>{label}</label>
+  return (
+    <div className="CheckboxField-Container flex justify-start items-center">
+      <div
+        className="CheckboxField mr-05"
+        role="checkbox"
+        tabIndex={0}
+        onKeyDown={keyDown}
+        onClick={toggle}
+      >
+        {checked && <Icon.Checkmark color={Color.primary} size={16} />}
       </div>
-    );
-  }
+      <label>{label}</label>
+    </div>
+  );
 }
