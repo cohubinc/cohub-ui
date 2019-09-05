38ddf7716b393b0066248871c2b38b601078ee53
Jack Spiva <johnspiva@gmail.com>
2019-09-05 16:46:51 -0500

    Add input tests (#42)
    * Get react testing library working
    
    * Add toggle test
    
    * Add checkbox tests
    
    * Use label text instead of test id
    
    * Update snaps

--- test/__tests__/Inputs/Checkbox.test.tsx
+++ test/__tests__/Inputs/Checkbox.test.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { mount } from "enzyme";
 
-import { Inputs } from "dist";
+import { Inputs, StoryHelpers } from "dist";
+import { fireEvent } from "@testing-library/dom";
+import { render } from "@testing-library/react";
 
 describe("CheckBox Input", () => {
   it("renders checked without crashing", async () => {
@@ -10,6 +12,7 @@ describe("CheckBox Input", () => {
         label="checkBox"
         input={{ value: true } as any}
         meta={{}}
+        id="checkbox_input"
       />
     );
 
@@ -22,9 +25,41 @@ describe("CheckBox Input", () => {
         label="checkBox"
         input={{ value: false } as any}
         meta={{}}
+        id="checkbox_input"
       />
     );
 
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("toggles on click", () => {
+    const { getByLabelText } = render(
+      <StoryHelpers.StateContainer defaultState="">
+        {({ state, setState }) => (
+          <Inputs.Checkbox
+            label="Checkbox Input"
+            key="selected"
+            id="checkbox_input"
+            input={{
+              name: "selected",
+              value: state,
+              onChange: () => setState(!state)
+            }}
+          />
+        )}
+      </StoryHelpers.StateContainer>
+    );
+
+    expect(getByLabelText("Checkbox Input")).toHaveAttribute(
+      "data-checked",
+      "false"
+    );
+
+    fireEvent.click(getByLabelText("Checkbox Input"));
+
+    expect(getByLabelText("Checkbox Input")).toHaveAttribute(
+      "data-checked",
+      "true"
+    );
   });
 });
--- test/__tests__/Inputs/Text.test.tsx
+++ test/__tests__/Inputs/Text.test.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { mount } from "enzyme";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Inputs } from "dist";
 
 describe("Text Input", () => {
@@ -7,5 +9,17 @@ describe("Text Input", () => {
     const wrapper = mount(<Inputs.Text />);
 
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("receives text", () => {
+    const { getByLabelText } = render(
+      <Inputs.Text data-qa="textInputId" label="Text Label" id="text_input" />
+    );
+
+    expect(getByLabelText("Text Label")).toBeEmpty();
+
+    userEvent.type(getByLabelText("Text Label"), "Hello, World!");
+
+    expect(getByLabelText("Text Label")).toHaveValue("Hello, World!");
   });
 });
