f83a308d0dcbe0fc53228fc6394bd6ea8a526a4c
Jack Spiva <johnspiva@gmail.com>
2019-10-03 11:48:10 -0500

    Update text area (#56)
    * Update text area
    
    * Omit props

--- src/components/Inputs/TextArea/index.tsx
+++ src/components/Inputs/TextArea/index.tsx
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
+import { FieldRenderProps } from "react-final-form";
 import { IStyleContainer } from "src/definitions/interfaces/IStyleContainer";
 import "./TextArea.scss";
 
+type FieldProps = FieldRenderProps<string, HTMLInputElement>;
+
 interface ITextAreaProps {
   readOnly?: boolean;
+  input?: Partial<FieldProps["input"]>;
+  meta?: FieldProps["meta"];
 }
 
 export type TTextAreaProps = ITextAreaProps &
@@ -12,7 +17,7 @@ export type TTextAreaProps = ITextAreaProps &
       React.TextareaHTMLAttributes<HTMLTextAreaElement>,
       HTMLTextAreaElement
     >,
-    "ref"
+    "ref" | "value" | "onChange" | "onBlur" | "onFocus"
   >;
 
 export default class TextArea extends Component<TTextAreaProps> {
