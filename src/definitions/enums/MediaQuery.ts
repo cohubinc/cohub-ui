beb9e14f21372873d1f00a1ac8db1b76768e5053
Elliott Roche <eroche91@gmail.com>
2019-10-21 09:56:59 -0500

    adding additional media queries and removing the minimum device pixel ratio check from mobile query

--- src/definitions/enums/MediaQuery.ts
+++ src/definitions/enums/MediaQuery.ts
@@ -1,8 +1,11 @@
 enum MediaQuery {
-  mobileMediaQuery = "(min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2)",
+  mobileMediaQuery = "(min-device-width: 320px) and (max-device-width: 480px)",
   tabletMediaQuery = "(min-device-width: 768px) and (max-device-width: 1024px)",
+  mobileOrTabletMediaQuery = "(max-device-width: 1224px)",
   desktopsLaptopsMediaQuery = "(min-device-width: 1224px)",
-  bigScreenMediaQuery = "(min-device-width: 1824px)"
+  bigScreenMediaQuery = "(min-device-width: 1824px)",
+  orientationPortrait = "orientation: portrait",
+  orientationLandscape = "orientation: lanscape"
 }
 
 export default MediaQuery;
--- src/hooks/useMediaQueries.ts
+++ src/hooks/useMediaQueries.ts
@@ -6,8 +6,19 @@ const useMediaQuery = (query: string): boolean => umq({ query });
 export default function useMediaQueries() {
   const isMobile = useMediaQuery(MediaQuery.mobileMediaQuery);
   const isTablet = useMediaQuery(MediaQuery.tabletMediaQuery);
+  const isMobileOrTablet = useMediaQuery(MediaQuery.mobileOrTabletMediaQuery);
   const isDesktop = useMediaQuery(MediaQuery.desktopsLaptopsMediaQuery);
   const isBigScreen = useMediaQuery(MediaQuery.bigScreenMediaQuery);
+  const isPortrait = useMediaQuery(MediaQuery.orientationPortrait);
+  const isLandscape = useMediaQuery(MediaQuery.orientationLandscape);
 
-  return { isMobile, isTablet, isDesktop, isBigScreen };
+  return {
+    isMobile,
+    isTablet,
+    isMobileOrTablet,
+    isDesktop,
+    isBigScreen,
+    isPortrait,
+    isLandscape
+  };
 }
