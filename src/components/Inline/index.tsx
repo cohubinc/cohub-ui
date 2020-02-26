b91e720e4c18dd1f802060ba75b228a3a044a0f2
Elliott Roche <eroche91@gmail.com>
2020-02-26 14:27:31 -0600

    added stack and inline components for vertical and horizontal grouping (#70)
    * added stack and inline components for vertical and horizontal grouping
    
    * added .npmrc file
    
    * changing alignmentSwitch to calculateAlignment

---
+++ src/components/Inline/index.tsx
@@ -0,0 +1,75 @@
+import React, { ReactNode } from "react";
+
+export interface IInlineProps {
+  children: ReactNode[] | ReactNode;
+  space?: number;
+  alignment?: "center" | "left" | "right";
+}
+
+export default function Inline({
+  space = 1,
+  children,
+  alignment = "left"
+}: IInlineProps) {
+  const calculateMargin = () => {
+    switch (alignment) {
+      case "center":
+        return {
+          marginLeft: `${space / 2}rem`,
+          marginRight: `${space / 2}rem`
+        };
+      case "left":
+        return {
+          marginRight: `${space}rem`
+        };
+      case "right":
+        return {
+          marginLeft: `${space}rem`
+        };
+      default:
+        return {
+          marginRight: `${space}rem`
+        };
+    }
+  };
+
+  const calculateAlignment = () => {
+    switch (alignment) {
+      case "center":
+        return { justifyContent: "center" };
+      case "left":
+        return { justifyContent: "flex-start" };
+      case "right":
+        return { justifyContent: "flex-end" };
+      default:
+        break;
+    }
+  };
+
+  const spacedChildren = () => {
+    if (Array.isArray(children)) {
+      return children.map((c, idx) => {
+        return (
+          <span key={idx} style={calculateMargin()}>
+            {c}
+          </span>
+        );
+      });
+    } else {
+      return <span style={calculateMargin()}>{children}</span>;
+    }
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        ...calculateAlignment()
+      }}
+    >
+      {spacedChildren()}
+    </div>
+  );
+}
